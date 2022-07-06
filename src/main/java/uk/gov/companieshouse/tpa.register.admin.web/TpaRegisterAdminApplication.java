a86a819120f8c28ed7b47c4a0a63ebc93f740dde
AlisonGriffiths <agriffiths1@companieshouse.gov.uk>
2022-07-06 17:32:09 +0100

    Initial commit for 3pa-register-admin-web

---
+++ src/main/java/uk/gov/companieshouse/tpa.register.admin.web/TpaRegisterAdminApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.paregisteradminweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpaRegisterAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpaRegisterAdminApplication.class, args);
+	}
+
+}
